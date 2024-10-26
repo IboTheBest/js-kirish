e3009c2ab8fbcb7a69f84466e11c591efff6467a
IboTheBest <cybergenyus@gmail.com>
2024-10-26 17:58:56 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var question1 = prompt("CSS nima")
+var question2 = prompt("CSS nima")
+var question3 = prompt("HTML ni to'liq nomi nima")
+var question4 = prompt("Boiler plate nima")
+var question5 = prompt("Rasm formatlari")
+var question6 = prompt("css ni htmlga ulash usullari")
+var question7 = prompt("rang turlari")
+var question8 = prompt("a href ni hususiyatlari")
+var question9 = prompt("100vh va 100vw ni farqi nima")
+var question10 = prompt("responsive qilishda ishlatiladigan css teg nomi va uni yozish ketma ketligi")
+var question11 = prompt("table yasash ketma ketligi")
+var question12 = prompt("form ning asosiy atributlari")
+var question13 = prompt("headinglar nima")
+var question14 = prompt("max-width bilan min-widthni farqi nima")
+var question15 = prompt("psevdo elementlar nima")
+
+console.log("answer -", question1);
+console.log("answer -", question2);
+console.log("answer -", question3);
+console.log("answer -", question4);
+console.log("answer -", question5);
+console.log("answer -", question6);
+console.log("answer -", question7);
+console.log("answer -", question8);
+console.log("answer -", question9);
+console.log("answer -", question10);
+console.log("answer -", question11);
+console.log("answer -", question12);
+console.log("answer -", question13);
+console.log("answer -", question14);
+console.log("answer -", question15);
